136381c2fe7cd745471de3e5d564735e3b1a368e
danielhsh <DanielHe@nvidia.com>
2025-07-10 09:38:10 +0300

    Add gusli service script
    Change-Id: I61c1791582888e8b5b07cc7a2cd679c7c0cc1a5a
    Signed-off-by: danielhsh <DanielHe@nvidia.com>

---
+++ 80scripts/service.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+function echo_green() {  echo -e "\e[0;32m$*\e[0m"; }
+function GUSLI() {
+	if [ $# -eq 0 ]; then
+		echo "params: show / clean / name / "
+	elif [[ $1 == sh* ]]; then
+		cmd="ps -eLo pid,ppid,comm,user | grep gusli"; echo_green $cmd; eval $cmd;
+		cmd='ll /dev/shm/gs*'; echo_green $cmd; eval $cmd;
+	elif [[ $1 == cl* ]]; then
+		ps_prg=`pgrep gusli`;	[ ! -z "$ps_prg" ] && sudo kill -9 ${ps_prg};
+		GUSLI show;
+	fi
+}
