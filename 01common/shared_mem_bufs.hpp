e12c46b5a6353c0b506a7263f75c524e6fe0cbf9
danielhsh <DanielHe@nvidia.com>
2025-08-03 17:07:57 +0300

    Bugfix in base_shm_element
    Change-Id: I523556d3f0bbb1c1bace8e8c267c79b29d5acd4c
    Signed-off-by: danielhsh <DanielHe@nvidia.com>

--- 01common/shared_mem_bufs.hpp
+++ 01common/shared_mem_bufs.hpp
@@ -70,7 +70,6 @@ class base_shm_element {			// Represents a unique shared mem region (with approp
 	const io_buffer_t get_buf() const { return io_buffer_t::construct(mem.get_buf(), mem.get_n_bytes()); }
  private:
 	base_shm_element& move(base_shm_element& o) noexcept {	// Move constructor
-		this->~base_shm_element();
 		mem = std::move(o.mem);
 		other_party_ptr = o.other_party_ptr;
 		buf_idx = o.buf_idx;
