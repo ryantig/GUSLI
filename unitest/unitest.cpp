ab1dc4acf11561045918f7378cd924a64df659e3
danielhsh <DanielHe@nvidia.com>
2025-10-16 14:10:39 +0300

    Cleanup prev commit
    Change-Id: I7ffb9457c44587821ed82bdb0fb5bb7c443fffd0
    Signed-off-by: danielhsh <DanielHe@nvidia.com>

--- unitest/unitest.cpp
+++ unitest/unitest.cpp
@@ -255,16 +255,17 @@ int base_lib_unitests(gusli::global_clnt_context& lib, int n_iter_race_tests = 1
 		my_io.clean_buf();
 	}
 
-	if (1) {	// Force Reset params
-		log_line("Set bdev to 10[blocks]");
-		const uint64_t new_size = 10 * bdi.block_size;
-		bdi.num_total_blocks = new_size;
-		if (0) bdi.set_leak_fs_file_on_close(true);
+	if (1) {	// Test Force Reset params
+		static constexpr const uint64_t new_size_blocks = 10;
+		log_line("Set bdev to %lu[blocks], each of %u[bytes]", new_size_blocks, bdi.block_size);
+		bdi.num_total_blocks = new_size_blocks;
+		bdi.set_leak_fs_file_on_close(true);
+		bdi.set_leak_fs_file_on_close();
 		my_assert(lib.bdev_override_info(bdev, bdi) == gusli::connect_rv::C_OK);
 		// Verify value was set, via get
 		bdi.clear();
 		my_assert(lib.bdev_get_info(bdev, bdi) == gusli::connect_rv::C_OK);
-		my_assert(bdi.num_total_blocks == new_size);
+		my_assert((bdi.num_total_blocks == new_size_blocks) && (bdi.bdev_descriptor == fd));
 		const uint64_t offset = 4;
 		my_assert(bdi.get_bdev_size() < (offset + data_len)); // Verify this io becomes illegal
 		my_io.io.params.init_1_rng(gusli::G_NOP, fd, 0, data_len, my_io.io_buf);
